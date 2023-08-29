6d16bc9f4d6fc1a057117f22e493b6980d76aafd
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-08-29 22:49:24 +0600

    partially multer and cloudinary tested

--- server/src/secret.js
+++ server/src/secret.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const port = process.env.PORT || 4000;
 const mongoUrl = process.env.MONGO_URL;
 const jwtSecret = process.env.JWT_SECRET;
+const cloudName = process.env.CLOUD_NAME;
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
 
 
-module.exports = {port, mongoUrl, jwtSecret};
+module.exports = {port, mongoUrl, jwtSecret, cloudName, apiKey, apiSecret};
